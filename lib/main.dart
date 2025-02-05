54db4b41a996fc959be14232a33da5ccb45282bb
Hugo Yang <hugoyang4567@gmail.com>
2025-02-05 14:41:02 -0500

    Displayed simulated weather information

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
                     ),
                   );
                 },
-          
+              ),
             ),
           ],
         ),
